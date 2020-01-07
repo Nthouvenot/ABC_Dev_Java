c033ddfe5fed5959361c8441d1e5311ae3e4ea41
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-07 10:55:50 +0100

    ajout de test de conversion

---
+++ Projet-test/test/com/crm/formation/TestConversion.java
@@ -0,0 +1,33 @@
+package com.crm.formation;
+
+public class TestConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//----------------------------------------		
+		//cast implicite
+		int x=0;
+		short z=12;
+		x=z;
+		System.out.println(x);
+		// cast explicite
+		int x2=65526;
+		short z2=0;
+		z2=(short) x2;
+		System.out.println(z2);
+		System.out.println();
+//----------------------------------------
+		long l1 = 123456789;
+		long l2 = 123456788;
+		float f1 = l1;
+		float f2 = l2;
+		
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println();
+		System.out.println(l1-l2);
+		System.out.println(f1-f2);
+//----------------------------------------
+	}
+
+}
