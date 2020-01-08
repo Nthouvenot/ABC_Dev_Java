d4ee26614ed8f8672b8f898d068515dd62b1a2b9
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-08 14:04:57 +0100

    refonte du programme

--- Projet-test/src/PerimetreRectangle.java
+++ Projet-test/src/PerimetreRectangle.java
@@ -1,42 +1,75 @@
 import java.util.Scanner;
-
-
-
+import com.crm.api.Calcul;
 public class PerimetreRectangle
 {
-	private static String refaire;
-	private static boolean prevInstance;
 	private static Scanner sc;
+	private static int longeur, largeur, angle;
+	private static double rayon;
 
 	PerimetreRectangle()
 	{
-		prevInstance = false;
-		refaire="O";
-		sc = new Scanner(System.in);
+		longeur = 0;
+		largeur = 0;
+		rayon = 0;
 	}
-	
 
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		int perimetre;
-		Calcul C = new Calcul();			
-		do
-		{
-			if(prevInstance == true)
-			{	
-				System.out.println("Voulez voulez vous refaire un calcul ? (O/N)");
-				refaire = sc.nextLine();
-			}
-			else
+		short choix = 0;
+		boolean ok = true;
+			Calcul C = new Calcul();
+			sc = new Scanner(System.in);
+			do
 			{
-				prevInstance = true;
+				choix = menu();
+				switch(choix)
+				{
+				case 1:
+					{
+						saisirRect();
+						System.out.println("Le périmétre du rectangle est : " + C.calculperimetre(longeur, largeur));
+						break;
+					}
+				case 2:
+					{
+						saisirSect();
+						System.out.println("L'aire du secteur circulaire est de : " + C.calculSecteurCirculaire(rayon, angle));
+						break;
+					}
+				case 3: ok=false; break;
+				}
 			}
-		perimetre = C.calculperimetre();
-		System.out.println("Le perimetre du rectangle est de " + perimetre);
-		}
-		while(refaire == "O");
+			while(ok);
 	}
+	
+	private static short menu()
+	{
+		short choix;
+		System.out.println("Menu :");
+		System.out.println("1) Calculer perimétre rectangle");
+		System.out.println("2) Calculer aire secteur ciculaire");
+		System.out.println("3) Quitter");
+		choix = sc.nextShort();
+		return choix;
+	}
+	
+	private static void saisirRect()
+	{
+		System.out.println("Entrez la longeur du rectangle");
+		longeur = sc.nextInt();
+		System.out.println("Entrez la largeur du rectangle");
+		largeur = sc.nextInt();
+	}
+	
+	private static void saisirSect()
+	{
+		System.out.println("Entrez le rayon du cercle");
+		rayon = sc.nextDouble();
+		System.out.println("Entrez l'angle du cercle");
+		angle = sc.nextInt();
+	}
+	
 	protected void finalize()
 	{
 		sc.close();
