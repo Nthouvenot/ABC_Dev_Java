a4797e9b53c9502230fb8884bbf414570abab915
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-08 15:19:44 +0100

    création d'une application de test

---
+++ Projet-test/src/CalculTest.java
@@ -0,0 +1,14 @@
+
+import com.crm.api.Utils;
+
+public class CalculTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(Utils.somme(4, 5));
+		System.out.println("Le ciel est " + Utils.couleur);
+		System.out.println(Utils.surfacecercle(3));
+	}
+
+}
