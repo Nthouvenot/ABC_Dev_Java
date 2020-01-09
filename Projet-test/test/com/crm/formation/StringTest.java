e3614ae57485d4891a80f1799e4d23f212a169c1
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-09 16:10:45 +0100

    Test des méthodes de la class String

--- Projet-test/test/com/crm/formation/StringTest.java
+++ Projet-test/test/com/crm/formation/StringTest.java
@@ -14,11 +14,24 @@ public class StringTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//concaténation
 		String str = "Bonjour";
 		String nom = "Nicolas";
 		str = str + " " + nom;
 		System.out.println(str);
-
-	}
-
+		//longeur d'une chaine
+		String str1 = "Java c'est génial !!!     ";
+		System.out.println(str1.length());
+		//Mettre en Majuscule
+		System.out.println(str1.toUpperCase());
+		//Suppression des blanc en début et fin de chaine
+		String str2 = "   " + str1 + "    ";
+		System.out.println(str2.trim());
+		//Remplacer des caractére
+		System.out.println(str1.replace('a', 'e'));
+		//Extraction d'une sous chaine
+		System.out.println(str1.substring(0,4));
+		}
+	
 }
