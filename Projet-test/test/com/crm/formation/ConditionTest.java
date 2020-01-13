783aa3adee2e67dbe4c448129c65ac2808591781
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-13 11:36:18 +0100

    test des instrutions conditionnelle

---
+++ Projet-test/test/com/crm/formation/ConditionTest.java
@@ -0,0 +1,21 @@
+package com.crm.formation;
+
+public class ConditionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean estVrai = true;
+		System.out.println("Début programme");
+		if(estVrai == true) {			
+			System.out.println("Yes");
+			System.out.println("WE CAN");
+		}
+		else {
+			System.out.println("NO");
+			System.out.println("WE CAN NOT");
+		}
+		System.out.println("Fin programme");
+	}
+
+}
