b572f0ea50d9e7c2b1f1182228ad16919f13d792
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-13 14:33:14 +0100

    remplacement texte du password et du login par des constantes

--- Projet-test/test/com/crm/formation/Password.java
+++ Projet-test/test/com/crm/formation/Password.java
@@ -6,6 +6,9 @@ public class Password {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		final String LOGIN = "CRM";
+		final String PASSWORD = "sdfsdfsdf";
+		
 		String login = "";
 		String password = "";
 		Scanner sc = new Scanner(System.in);
@@ -13,8 +16,10 @@ public class Password {
 		login = sc.nextLine();
 		System.out.println("Entrez votre mot de passe");
 		password = sc.nextLine();
-		if(login.equals("CRM") && password.equals("sdfsdfsdf")) System.out.println("Login sucess");
-		else System.out.println("Login Failed");
+		if (login.equals(LOGIN) && password.equals(PASSWORD))
+			System.out.println("Login sucess");
+		else
+			System.out.println("Login Failed");
 		sc.close();
 	}
 
