3face193e45dd9f0aa2381282e43a35acc94a747
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-14 11:35:44 +0100

    test des opérateurs ternaire

---
+++ Projet-test/test/com/crm/formation/OperateurTernaire.java
@@ -0,0 +1,14 @@
+package com.crm.formation;
+
+public class OperateurTernaire {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nbr1 = 2;
+		int nbr2 = 3;
+		boolean estVrai = nbr1>nbr2 ? true : false;
+		System.out.println(estVrai);
+		
+	}
+
+}
