e6b5ea1b6fd7c4a36d4ae00127ba4283549f1631
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-16 11:00:01 +0100

    test boucle imbriqué et algo avec l'affichage d'un triangle

---
+++ Projet-test/src/DessinerUnTriangleRectangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DessinerUnTriangleRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int longeur = 0, i = 0, i2 = 0, ligne = 1;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez une longeur du triangle");
+		longeur = sc.nextInt();
+		sc.close();
+		System.out.println("Le  triangle de longeur " + longeur + " :");
+		for (i2 = 0; i2 < longeur; i2++) {
+			for (i = 0; i < ligne; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			ligne++;
+		}
+	}
+
+}
