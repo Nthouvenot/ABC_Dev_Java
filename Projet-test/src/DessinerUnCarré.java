48906dea271a791185b27ea36bdb9c44e7844f92
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-16 10:59:11 +0100

    test boucle imbriqué et algo avec l'affichage d'un carré

---
+++ Projet-test/src/DessinerUnCarré.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DessinerUnCarré {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int longeur = 0, i = 0, i2 = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez une longeur");
+		longeur = sc.nextInt();
+		sc.close();
+		System.out.println("Le carré de longeur " + longeur + " :");
+		for (i2 = 0; i2 < longeur; i2++) {
+			for (i = 0; i < longeur; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
