3ab4f9dd93e0ca39a7edc6dbb375896c7f63f485
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-16 14:14:16 +0100

    Test de l'instruction return

---
+++ Projet-test/test/com/crm/formation/InstructionReturnTest.java
@@ -0,0 +1,21 @@
+package com.crm.formation;
+
+public class InstructionReturnTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nombre = 10;
+		afficherListeNombre(nombre);
+	}
+
+	public static void afficherListeNombre(int nb) {
+		System.out.println("Début méthode aficherListeNombre");
+
+		for (int i = 0; i < nb; i++) {
+			System.out.println(i);
+			if (i == 5)
+				return;
+		}
+		System.out.println("Fin méthode afficherListeNombre");
+	}
+}
