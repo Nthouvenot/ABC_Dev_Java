2b9b66ad761adbd3c0e977619c5a18117e38d916
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-28 11:38:08 +0100

    Test des argument fonction main

---
+++ Projet-test/src/com/crm/api/ArgumentMain.java
@@ -0,0 +1,14 @@
+package com.crm.api;
+
+public class ArgumentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("La taille du tableau est : " + args.length);
+		
+		for(String str : args) {
+			System.out.println(str);
+		}
+	}
+
+}
