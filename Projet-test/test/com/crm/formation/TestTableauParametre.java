e9dd41398367a23b79269a0b04c115d8d03a39f7
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2020-01-28 11:39:17 +0100

    Test tableau paramétre + tableau dynamique (vector)

---
+++ Projet-test/test/com/crm/formation/TestTableauParametre.java
@@ -0,0 +1,44 @@
+package com.crm.formation;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class TestTableauParametre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<Integer> tab = new Vector<Integer>(1);
+		String scan="";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("tapez q une fois que vous avez saisi tout les éléments");
+		do {
+		System.out.println("Entrez un entier");
+		scan = sc.nextLine();
+		
+		if(!scan.contentEquals("q")) {
+			try
+			{
+				tab.add(Integer.parseInt(scan));
+			}
+			catch(Exception e)
+			{
+				System.out.println("Ce n'est pas un nombre");
+			}
+		}
+		}
+		while(!scan.contentEquals("q"));
+		System.out.println("resultat de l'additon de tout les éléments du tableau " + calculElementTableau(tab));
+		sc.close();
+	}
+	
+	public static int calculElementTableau(Vector<Integer> tab)
+	{
+		int result=0;
+		for(int i=0;i<tab.size();i++) {
+			result += (int)tab.get(i);
+		}
+			return result;
+		
+	}
+
+}
